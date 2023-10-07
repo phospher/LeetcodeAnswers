f4f4d4da6b373e54191a9ea11dcf3a9baae2045f
phospher <phospher@qq.com>
2023-10-07 17:28:31 +0800

    update

--- src/22-solution.cpp
+++ src/22-solution.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 class Solution
 {
-    void backtrack(vector<string> &ans, string &cur, int open, int close, int n)
+public:
+    void process(vector<string> &ans, string cur, int open, int close, int n)
     {
         if (cur.size() == n * 2)
         {
@@ -15,24 +16,18 @@ class Solution
         }
         if (open < n)
         {
-            cur.push_back('(');
-            backtrack(ans, cur, open + 1, close, n);
-            cur.pop_back();
+            process(ans, cur + "(", open + 1, close, n);
         }
         if (close < open)
         {
-            cur.push_back(')');
-            backtrack(ans, cur, open, close + 1, n);
-            cur.pop_back();
+            process(ans, cur + ")", open, close + 1, n);
         }
     }
-
-public:
     vector<string> generateParenthesis(int n)
     {
         vector<string> result;
         string current;
-        backtrack(result, current, 0, 0, n);
+        process(result, current, 0, 0, n);
         return result;
     }
 };
